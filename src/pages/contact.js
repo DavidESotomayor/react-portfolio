a2694c4bac5fb75838e3beea0a1de9bdb38c430c
David Sotomayor <david.elijah.sotomayor@gmail.com>
2021-04-06 22:59:43 -0500

    update portfolio projects page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,31 +2,31 @@ import React from 'react'
 
 const Contact = () => {
     return (
-    <div class="container ds-container">
-        <div class="row">
-            <div class="col-lg-8">
+    <div className="container ds-container">
+        <div className="row">
+            <div className="col-lg-8">
                 <h2>Contact</h2>
             </div>
         </div>
-        <div class="col-lg-8 p-0">
+        <div className="col-lg-8 p-0">
             <hr />
         </div>
-        <div class="row">
-            <form action="https://formspree.io/f/xbjpkynk" class="col-lg-8" method="POST">
-                <div class="form-group">
-                    <label for="inputName">Name</label>
-                    <input type="text" name="name" class="form-control" id="inputName" placeholder="Name" />
+        <div className="row">
+            <form action="https://formspree.io/f/xbjpkynk" className="col-lg-8" method="POST">
+                <div className="form-group">
+                    <label htmlFor="inputName">Name</label>
+                    <input type="text" name="name" className="form-control" id="inputName" placeholder="Name" />
                 </div>
-                <div class="form-group">
-                    <label for="inputEmail">Email</label>
-                    <input type="email" name="_replyto" class="form-control" id="inputEmail" placeholder="Email" />
+                <div className="form-group">
+                    <label htmlFor="inputEmail">Email</label>
+                    <input type="email" name="_replyto" className="form-control" id="inputEmail" placeholder="Email" />
                 </div>
-                <div class="form-group">
-                    <label for="messageTextArea">Message</label>
-                    <textarea class="form-control" name="message" id="messageTextArea" rows="3"
+                <div className="form-group">
+                    <label htmlFor="messageTextArea">Message</label>
+                    <textarea className="form-control" name="message" id="messageTextArea" rows="3"
                         placeholder="Message"></textarea>
                 </div>
-                <button type="submit" value="Send" class="btn btn-primary mb-3 buttonStyle">Submit</button>
+                <button type="submit" value="Send" className="btn btn-primary mb-3 buttonStyle">Submit</button>
             </form>
         </div>
     </div>
