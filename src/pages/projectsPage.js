8b6807716f3c854606206b1302ecd7ee5539e1f5
David Sotomayor <david.elijah.sotomayor@gmail.com>
2021-04-08 12:25:19 -0500

    move assets to src; fix image path

--- src/pages/projectsPage.js
+++ src/pages/projectsPage.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import Project from '../components/Project'
+import Jeopardy from './../assets/images/jeopardy-trivia-game.png'
+import EatDaBurger from "./../assets/images/Eat-Da-Burger.png";
+import EmployeeTracker from "./../assets/images/employee-tracker.gif";
+import ExpressNoteTakerRepository from "./../assets/images/note_taker.png";
+import EmployeeSummary from "./../assets/images/employee-summary.png";
+import MoviesOnTheRocks from "./../assets/images/MoviesontheRocks.jpg";
+import WeatherDashboard from "./../assets/images/weather-dashboard.png";
+import WorkdayScheduler from "./../assets/images/workdayscheduler.png";
+import CodeQuiz from "./../assets/images/code_quiz.png";
+import PasswordGenerator from "./../assets/images/Password_Generator.png";
 
 const Projects = () => {
     return (
@@ -18,7 +28,7 @@ const Projects = () => {
                         <Project
                             deployURL={"https://shielded-fjord-19309.herokuapp.com/"}
                             title={"Jeopardy Trivia Game"}
-                            img={"./assets/images/jeopardy-trivia-game.png"}
+                            img={Jeopardy}
                             repoURL={"https://github.com/Geoff7709/jeopardy_trivia_game"}
                             description={"Some quick example text to build on the card title and make up the bulk of the card's content."}
                         />
@@ -26,63 +36,63 @@ const Projects = () => {
                             deployURL={"https://pacific-peak-81843.herokuapp.com/"}
                             title={"Eat-Da-Burger"}
                             repoUrl={"https://github.com/DavidESotomayor/burger"}
-                            img={"./assets/images/Eat-Da-Burger.png"}
+                            img={EatDaBurger}
                             description={"Some quick example text to build on the card title and make up the bulk of the card's content."}
                         />
                         <Project
                             deployURL={"https://github.com/DavidESotomayor/employee-tracker"}
                             title={"Employee Tracker"}
                             repoUrl={"https://github.com/DavidESotomayor/employee-tracker"}
-                            img={"./assets/images/employee-tracker.gif"}
+                            img={EmployeeTracker}
                             description={"Some quick example text to build on the card title and make up the bulk of the card's content."}
                         />
                         <Project
                             deployURL={"https://agile-reef-85586.herokuapp.com/"}
                             title={"Express Note Taker Repository"}
                             repoUrl={"https://github.com/DavidESotomayor/express-note-taker"}
-                            img={"./assets/images/note_taker.png"}
+                            img={ExpressNoteTakerRepository}
                             description={"Some quick example text to build on the card title and make up the bulk of the card's content."}
                         />
                         <Project
                             deployURL={"https://github.com/DavidESotomayor/template-engine_employee-summary"}
                             title={"Template Engine: Employee Summary"}
                             repoUrl={"https://github.com/DavidESotomayor/template-engine_employee-summary"}
-                            img={"./assets/images/employee-summary.png"}
+                            img={EmployeeSummary}
                             description={"Some quick example text to build on the card title and make up the bulk of the card's content."}
                         />
                         <Project
                             deployURL={"https://mklakhan.github.io/movies-on-the-rocks/"}
                             title={"Movies On The Rocks"}
                             repoUrl={"https://github.com/mklakhan/movies-on-the-rocks"}
-                            img={"./assets/images/MoviesontheRocks.jpg"}
+                            img={MoviesOnTheRocks}
                             description={"Some quick example text to build on the card title and make up the bulk of the card's content."}
                         />
                         <Project
                             deployURL={"https://davidesotomayor.github.io/weather-dashboard/"}
                             title={"Weather Dashboard"}
                             repoUrl={"https://github.com/DavidESotomayor/weather-dashboard"}
-                            img={"./assets/images/weather-dashboard.png"}
+                            img={WeatherDashboard}
                             description={"Some quick example text to build on the card title and make up the bulk of the card's content."}
                         />
                         <Project
                             deployURL={"https://davidesotomayor.github.io/work-day-scheduler/"}
                             title={"Workday Scheduler"}
                             repoUrl={"https://github.com/DavidESotomayor/work-day-scheduler"}
-                            img={"./assets/images/workdayscheduler.png"}
+                            img={WorkdayScheduler}
                             description={"Some quick example text to build on the card title and make up the bulk of the card's content."}
                         />
                         <Project
                             deployURL={"https://davidesotomayor.github.io/code-quiz/"}
                             title={"Code Quiz"}
                             repoUrl={"https://github.com/DavidESotomayor/code-quiz"}
-                            img={"./assets/images/code_quiz.png"}
+                            img={CodeQuiz}
                             description={"Some quick example text to build on the card title and make up the bulk of the card's content."}
                         />
                         <Project
                             deployURL={"https://davidesotomayor.github.io/Password_Generator/"}
                             title={"Password Generator"}
                             repoUrl={"https://github.com/DavidESotomayor/Password_Generator"}
-                            img={"./assets/images/Password_Generator.png"}
+                            img={PasswordGenerator}
                             description={"Some quick example text to build on the card title and make up the bulk of the card's content."}
                         />
                     </div>
